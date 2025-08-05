d72ddba8193bb6677ac2cd9e36fb522ed93d233d
azeezshaik599 <azeezshaik599@gmail.com>
2025-08-05 16:14:19 +0530

    v1

---
+++ src/main/java/klu/CalcpApplication.java
@@ -0,0 +1,13 @@
+package klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcpApplication.class, args);
+	}
+
+}
